8db7f3587153012c4772cd886c9ed4fe52193fa1
Ivan Buldakov <iv081198@yandex.ru>
2020-06-06 18:55:09 +0300

    update

--- src/pages/TreatmentList.vue
+++ src/pages/TreatmentList.vue
@@ -71,7 +71,7 @@
                 <div class="col">
                   <div class="text-h6" style="font-size: 18px">
                     <router-link
-                      to="/leader-detail">
+                      to="/home/leader-detail">
                       Алексей Петров
                     </router-link>
                   </div>

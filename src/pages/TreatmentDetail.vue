a79537ae392adf7acd609a2159eaa19b59599f01
Ivan Buldakov <iv081198@yandex.ru>
2020-06-07 01:19:43 +0300

    add timeline

--- src/pages/TreatmentDetail.vue
+++ src/pages/TreatmentDetail.vue
@@ -66,6 +66,67 @@
           </iframe>
         </div>
       </q-card-section>
+      <q-card-section>
+        <q-timeline color="secondary">
+          <q-timeline-entry heading>
+            История обращения
+          </q-timeline-entry>
+
+          <q-timeline-entry
+            title="Заголовок события 1"
+            subtitle="5 июня 2020"
+          >
+            <div>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore magna
+               aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                 Duis aute irure dolor in reprehenderit in voluptate
+                  velit esse cillum dolore eu fugiat nulla pariatur.
+                   Excepteur sint occaecat cupidatat non proident,
+                    sunt in culpa qui officia deserunt mollit anim
+                     id est laborum.
+            </div>
+          </q-timeline-entry>
+
+          <q-timeline-entry
+            title="Заголовок события 2"
+            subtitle="6 июня 2020"
+            icon="delete"
+          >
+            <div>
+              Lorem ipsum dolor sit amet, consectetur
+               adipisicing elit, sed do eiusmod tempor
+                incididunt ut labore et dolore magna aliqua.
+                 Ut enim ad minim veniam, quis nostrud exercitation
+                  ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                   Duis aute irure dolor in reprehenderit in voluptate
+                    velit esse cillum dolore eu fugiat nulla pariatur.
+                     Excepteur sint occaecat cupidatat non proident,
+                      sunt in culpa qui officia deserunt mollit
+                       anim id est laborum.
+            </div>
+          </q-timeline-entry>
+          <q-timeline-entry
+            title="Заголовок события 3"
+            subtitle="7 июня 2020"
+            icon="add"
+          >
+            <div>
+              Lorem ipsum dolor sit amet, consectetur
+               adipisicing elit, sed do eiusmod tempor
+                incididunt ut labore et dolore magna aliqua.
+                 Ut enim ad minim veniam, quis nostrud exercitation
+                  ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                   Duis aute irure dolor in reprehenderit in voluptate
+                    velit esse cillum dolore eu fugiat nulla pariatur.
+                     Excepteur sint occaecat cupidatat non proident,
+                      sunt in culpa qui officia deserunt mollit
+                       anim id est laborum.
+            </div>
+          </q-timeline-entry>
+        </q-timeline>
+      </q-card-section>
     </q-card>
   </q-page>
 </template>

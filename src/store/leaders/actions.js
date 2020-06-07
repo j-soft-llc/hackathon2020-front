c6c4f1743fb27916d0cd47eefc9252b203829235
Ivan Buldakov <iv081198@yandex.ru>
2020-06-07 03:16:00 +0300

    update action getLeaders

--- src/store/leaders/actions.js
+++ src/store/leaders/actions.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
-export async function getLeaders() {
-  const items = await axios.get('/api/leaders/');
+export async function getLeaders(context) {
+  const token = localStorage.getItem('token');
+  const items = await axios.get('/api/leaders/', {
+    headers: {
+      Authorization: `Token ${token}`,
+    },
+  });
   console.log(items);
-  // await context.commit('setItems', token.data);
+  await context.commit('setItems', token.data);
 }
